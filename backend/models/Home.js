3849ed837a90d147199b1b54d8d322cb61928f14
ultimatehrishi <hrishikeshwaikar@gmail.com>
2019-05-03 03:50:13 -0700

    Setup mongodb server on atlas

--- backend/models/Home.js
+++ backend/models/Home.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema;
 
 const HomeSchema = new Schema({
     name: String,
-    passcode: String
+    passcode: String,
+    createdBy: { type: Schema.Types.ObjectId, ref: 'Person' }
 }, { timestamps: true });
 
 module.exports = mongoose.model("Home", HomeSchema);
--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,11 @@ const home_router = require('./routes/home');
 
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+require('./db');
+
 app.use(bodyParser.json());
 app.use(cors());
+
 
 app.use(
     bodyParser.urlencoded({
